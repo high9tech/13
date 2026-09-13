ed37bf019ab5f51f0a8220c6e150faaebd37a5ed
high9tech <hightechps96@gmail.com>
2026-09-13 19:48:10 +0300

    Refactor int64 functions for improved clarity

--- int64.js
+++ int64.js
@@ -1,65 +1,76 @@
 /* HIGH TECH PS - Int64 Precision Handling Header */
-function zeroFill(number, width) {
-    width -= number.toString().length;
-    if (width > 0) {
-        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
-    }
-    return number + "";
-}
 
-function int64(low, hi) {
+function int64(low, hi = 0) {
     this.low = (low >>> 0);
     this.hi = (hi >>> 0);
     this.backing = null;
 
     this.add32inplace = function (val) {
-        let new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
-        let new_hi = (this.hi >>> 0);
-        if (new_lo < this.low) { new_hi++; }
+        let val32 = val >>> 0;
+        let new_lo = ((this.low >>> 0) + val32) >>> 0;
+        let new_hi = this.hi >>> 0;
+        if (new_lo < (this.low >>> 0)) { 
+            new_hi = (new_hi + 1) >>> 0; 
+        }
         this.hi = new_hi;
         this.low = new_lo;
     };
 
     this.add32 = function (val) {
-        let new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
-        let new_hi = (this.hi >>> 0);
-        if (new_lo < this.low) { new_hi++; }
+        let val32 = val >>> 0;
+        let low32 = this.low >>> 0;
+        let new_lo = (low32 + val32) >>> 0;
+        let new_hi = this.hi >>> 0;
+        if (new_lo < low32) { 
+            new_hi = (new_hi + 1) >>> 0; 
+        }
         return new int64(new_lo, new_hi);
     };
 
     this.sub32 = function (val) {
-        let new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
-        let new_hi = (this.hi >>> 0);
-        if (new_lo > (this.low & 0xFFFFFFFF)) { new_hi--; }
+        let val32 = val >>> 0;
+        let low32 = this.low >>> 0;
+        let new_lo = (low32 - val32) >>> 0;
+        let new_hi = this.hi >>> 0;
+        if (val32 > low32) { 
+            new_hi = (new_hi - 1) >>> 0; 
+        }
         return new int64(new_lo, new_hi);
     };
 
     this.sub32inplace = function (val) {
-        let new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
-        let new_hi = (this.hi >>> 0);
-        if (new_lo > (this.low & 0xFFFFFFFF)) { new_hi--; }
+        let val32 = val >>> 0;
+        let low32 = this.low >>> 0;
+        let new_lo = (low32 - val32) >>> 0;
+        let new_hi = this.hi >>> 0;
+        if (val32 > low32) { 
+            new_hi = (new_hi - 1) >>> 0; 
+        }
         this.hi = new_hi;
         this.low = new_lo;
     };
 
     this.and32 = function (val) {
-        return new int64(this.low & val, this.hi);
+        return new int64((this.low & val) >>> 0, this.hi);
     };
 
     this.and64 = function (vallo, valhi) {
-        return new int64(this.low & vallo, this.hi & valhi);
+        return new int64((this.low & vallo) >>> 0, (this.hi & valhi) >>> 0);
     };
 
     this.toString = function (radix = 16) {
         let lo_str = (this.low >>> 0).toString(radix);
         let hi_str = (this.hi >>> 0).toString(radix);
-        if (this.hi == 0) {
+        
+        if ((this.hi >>> 0) === 0) {
             return lo_str;
-        } else {
-            const width = radix === 16 ? 8 : Math.ceil(32 / Math.log2(radix));
-            lo_str = zeroFill(lo_str, width);
         }
-        return hi_str + lo_str;
+        
+        if (radix === 16) {
+            return hi_str + lo_str.padStart(8, '0');
+        }
+        
+        return (BigInt(this.hi >>> 0) * 4294967296n + BigInt(this.low >>> 0)).toString(radix);
     };
 
     return this;
